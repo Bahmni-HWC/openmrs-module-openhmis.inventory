040ed3d82ca6be051fa2fb7a9fadad74bdb7cbba
deepthi-m <deepthi.mantena@thoughtworks.com>
2023-06-14 12:16:10 +0530

    Trello-155[Deepthi/Keerthana] | In progress still need refactoring

--- api/src/main/java/org/openmrs/module/openhmis/inventory/InventoryModuleActivator.java
+++ api/src/main/java/org/openmrs/module/openhmis/inventory/InventoryModuleActivator.java
@@ -17,44 +17,36 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import org.openmrs.Concept;
-import org.openmrs.User;
 import org.openmrs.api.UserService;
 import org.openmrs.api.impl.UserServiceImpl;
 import org.openmrs.module.BaseModuleActivator;
 import org.openmrs.module.openhmis.inventory.api.IItemDataService;
-import org.openmrs.module.openhmis.inventory.api.impl.ItemDataServiceImpl;
 import org.openmrs.module.openhmis.inventory.api.model.Department;
 import org.openmrs.module.openhmis.inventory.api.model.Item;
 import org.openmrs.module.openhmis.inventory.api.model.ItemPrice;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.web.client.RestTemplate;
+
+import org.springframework.transaction.annotation.Transactional;
 
 import java.io.*;
 
 import java.math.BigDecimal;
-import java.nio.charset.StandardCharsets;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 /**
  * This class contains the logic that is run every time this module is either started or stopped.
  */
+
 public class InventoryModuleActivator extends BaseModuleActivator {
 
 	private static final Log LOG = LogFactory.getLog(InventoryModuleActivator.class);
 
-	/*@Autowired
-	private IItemDataService dataService;*/
+	@Autowired
+	private IItemDataService invItemService;
 
 	/*Logger logger = (Logger) LogManager.getLogger(InventoryModuleActivator.class);*/
 
@@ -70,6 +62,8 @@ public class InventoryModuleActivator extends BaseModuleActivator {
 	 * @see BaseModuleActivator#started()
 	 */
 	@Override
+	@Autowired
+	@Transactional
 	public void started() {
 		try {
 			getInventorydata();
@@ -91,7 +85,6 @@ public class InventoryModuleActivator extends BaseModuleActivator {
 	}
 
 	public void getInventorydata() throws IOException {
-
 		// private final String baseUrl = 'http://localhost/openmrs/ws/rest/v2/inventory/item';
 		final InputStream filepathabsolute = getClass().getClassLoader().getResourceAsStream("inv_item.csv");
 		//CSVFile persistedUploadedFile = writeToLocalFile(file, filesDirectory);
@@ -113,23 +106,14 @@ public class InventoryModuleActivator extends BaseModuleActivator {
 			lineData = sc.nextLine();
 			LOG.error("inside while............" + lineData);
 			addItem(lineData);
-			/*items.add(sc.toString());
-			LOG.error("Adding items............." + items);*/
-			//System.out.print(sc.next());  //find and returns the next complete token from this scanner
-		}
 
+		}
 		//invokeUsingRestTemplate();
 		sc.close(); //closes the scanner
 	}
 
 	public void addItem(String lineData) {
 
-		//String url = "http://openmrs:8080/openmrs/ws/rest/v2/inventory/item";
-		/*String username = "superman";
-		String password = "Admin123";
-		String jsonData =
-		        "{\"name\":\"crocin10\",\"department\":\"2c37c6c1-64c0-4ee7-ae30-bc068b6deae1\",\"hasExpiration\":false,\"defaultExpirationPeriod\":null,\"concept\":\"\",\"hasPhysicalInventory\":false,\"minimumQuantity\":null,\"buyingPrice\":null,\"codes\":[],\"prices\":[{\"name\":\"test\",\"price\":10}],\"defaultPrice\":{\"name\":\"test\",\"price\":10}}";
-		*/
 		Item newItem = new Item();
 		UserService userService = new UserServiceImpl();
 
@@ -144,8 +128,7 @@ public class InventoryModuleActivator extends BaseModuleActivator {
 		newItem.setHasExpiration(Boolean.valueOf(lineValues[5]));
 		concept.setConceptId(Integer.valueOf(lineValues[6]));
 		newItem.setConcept(concept);
-		User user = userService.getUser(Integer.valueOf(lineValues[7]));
-		newItem.setCreator(user);
+
 		try {
 			newItem.setDateCreated(sdf.parse(lineValues[8]));
 		} catch (ParseException e) {
@@ -157,10 +140,8 @@ public class InventoryModuleActivator extends BaseModuleActivator {
 		price.setPrice(BigDecimal.valueOf(Long.parseLong(lineValues[4])));
 		newItem.setDefaultPrice(price);
 
-		ItemDataServiceImpl dataService = new ItemDataServiceImpl();
-		LOG.error("newItem......" + newItem);
-
-		dataService.save(newItem);
+		//dataService.setRepository(new IHibernateRepository());
+		invItemService.save(newItem);
 
 	}
 }
